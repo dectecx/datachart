dd8deb7c6b62da84525b39a25382c008cce65828
dectecx <75653667+dectecx@users.noreply.github.com>
2024-05-31 16:08:09 +0800

    🎨Design dropdown component

--- vite-project/src/components/navigation.vue
+++ vite-project/src/components/navigation.vue
@@ -1,7 +1,18 @@
 <template>
   <nav class="fixed w-full h-12 bg-gray-800 px-4 z-[9998]">
     <div class="flex w-full h-full justify-between items-center">
-      <router-link :to="{ name: RouterName.HOME }" class="text-white text-xl font-bold">Data Chart</router-link>
+      <div>
+        <router-link :to="{ name: RouterName.HOME }" class="text-white text-xl font-bold">Data Chart</router-link>
+      </div>
+      <div class="flex justify-center">
+        <Dropdown />
+        <Dropdown />
+        <Dropdown />
+        <Dropdown />
+        <Dropdown />
+        <Dropdown />
+        <Dropdown />
+      </div>
       <div>
         <router-link :to="{ name: RouterName.HOME }" class="text-white mr-4">Chart1</router-link>
         <router-link :to="{ name: RouterName.HOME }" class="text-white">Chart2</router-link>
@@ -11,5 +22,6 @@
 </template>
 
 <script setup lang="ts">
+import Dropdown from '@/components/dropdown.vue';
 import { RouterName } from '@/common/enums/routerName';
 </script>
